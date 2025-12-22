ebe5e9e7ccfbda5735c04631aa97ba66cf2c62af
Anurag Patel <131543420+AnuragPatel94@users.noreply.github.com>
2025-12-22 12:09:55 +0530

    Update SDET RestAssured.java

--- SDET RestAssured.java
+++ SDET RestAssured.java
@@ -85,15 +85,120 @@ public class cookiesDemo{
 		                   String cookie_value=res.getCookie(k);
 		                   System.out.println(k+"   "+cookie_value);}
 
+
+//VALIDATE RESPONSE DATA(JSON)
+	  public class parsingJSONResponseData{
+	  @Test
+		  void testJSONResponse(){
+			  given()
+				.contentType(content-type.JSON)
+			  .when()
+				.get("url")
+			  .then()
+				  .status code(200)
+				  .header("content-type","application/JSON;charset=utf-8")
+				  .body("JSON Path",equalTo("value"));
+		  }
+				  
 //VALIDATE HEADERS
-  void testHeaders() {
+  public class validateHeaders{
+   @Test
+    void testHeaders() {
 		 given()
-     .when()
+         .when()
 		    .post("https://www.google.com/")
-		.then()
-		 .header("Content-Type","text/html; charset=UTF-8")
-     .header("Content-Encoding","gzip")
-     .header("Server","gws");
+		 .then()
+		   .header("Content-Type","text/html; charset=UTF-8")
+           .header("Content-Encoding","gzip")
+           .header("Server","gws");
 			
 	}
+
+//HOW TO UPLOAD FILE
+  public class fileuploadanddownload{
+   @Test
+    void test singlefileUpload() {File myfile=newfile("filepath")
+		 given()
+		    .multipart("file",myfile)
+		    .content Type("multipart/from")
+         .when()
+		    .post("url")
+		 .then()
+		   .status code(200)
+		   .body("fileName",equalTo("Test1"))
+		   .log().all();
+			
+	}  
+
+
+//HOW TO VALIDATE SCHEMA
+  public class JSONSchemaValidation{
+   @Test
+    void test schemavalidation() {
+		 given()
+         .when()
+		    .get("url")
+		 .then()
+		   .assertThat().body(JSONSchemaValidation.matchesJSONSchemaInclassPath("FileName"));
+		   .log().all();
+			
+	}  
+
+
+//HOW TO HANDLE AUTHENTICTION
+  public class Authentication{
+   @Test-1
+    void test BasicAuth() {
+		 given()
+			.auth().basic("username","pswd")
+			       .digest("username","pswd")
+			       .preemptive("username","pswd")
+         .when()
+		    .get("url")
+		 .then()
+		   .statusCode(200)
+		   .body("authenticated".equalTo(true));
+		   .log().all();
+			
+	}  
+
+
+  @Test-2
+    void test BearerTokenAuth() {
+	  string BrToken='Token'
+		 given()
+			.headrs("Authentication","Bearer"+BrToken")	     
+         .when()
+		    .get("url")
+		 .then()
+		   .statusCode(200)
+		   .log().all();
+			
+	}  
+
+  @Test-3 
+ void test OAuth2Authentication() {
+		 given()
+			.auth()Oauth2("accessToken")	     
+         .when()
+		    .get("url")
+		 .then()
+		   .statusCode(200)
+		   .log().all();
+			
+	}  
+
+
+
+  @Test-4 
+ void test APIAuthentication() {
+		 given()
+			.queryparam("api key","API")	     
+         .when()
+		    .get("url")
+		 .then()
+		   .statusCode(200)
+		   .log().all();
+			
+	}  
 		 
